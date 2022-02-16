0200b27847da5ca32f35edf413022127d96d8c50
Albertino Augusto <albertinoaugusto.it@gmail.com>
2022-02-17 01:39:42 +0200

    Customizing Tool Scheme and adding mongoose to package.json

--- src/models/Tool.js
+++ src/models/Tool.js
@@ -1,47 +1,20 @@
-const firedata = require('../datastore/database');
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
-const getAdmin = (id) => {
-  return firedata.firestore.collection("admins").doc(id).get();
-}
+const blogSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  snippet: {
+    type: String,
+    required: true,
+  },
+  body: {
+    type: String,
+    required: true
+  },
+}, { timestamps: true });
 
-const getRequisitions= () => {
-  return firedata.firestore.collection("requisitions").orderBy('count', 'desc').get();
-}
-
-const getUnckedRequisitions = () => {
-  return firedata.firestore.collection("requisitions").where("status", "==", false).get();
-}
-
-const getInvoices = () => {
-  return firedata.firestore.collection("invoices").get();
-}
-
-const getInvoicesByRequisitionId = (requisitionId) => {
-  return firedata.firestore.collection("invoices").where("requisitionId", "==", requisitionId).get();
-}
-
-const getParticipants = () => {
-  return firedata.firestore.collection("participants").get();
-}
-
-const getViews = () => {
-  return firedata.firestore.collection("refreshes").doc("A9lDah36qjNXcWCfMmMU").get();
-}
-
-const checkRequisition = (requisitionId) => {
-  return firedata.firestore.collection("requisitions").doc(requisitionId).update({ status: true });
-}
-
-const checkInvoice = (requisitionId) => {
-  return firedata.firestore.collection("invoices").doc(requisitionId).update({ status: true });
-}
-
-const deleteParticipant = (id) => {
-  return firedata.admin.auth().deleteUser(id);
-}
-
-const deactivateParticipant = (id) => {
-  return firedata.firestore.collection("participants").doc(id).update({ active: false });
-}
-
-module.exports = { getAdmin, getRequisitions, getUnckedRequisitions, getInvoices, getInvoicesByRequisitionId, getParticipants, getViews, checkRequisition, checkInvoice, deleteParticipant, deactivateParticipant}
+const Tool = mongoose.model('Tool', blogSchema);
+module.exports = Tool;
--- src/routes/tool.js
+++ src/routes/tool.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 const appAdmin = require('../app/admin');
 
-router.get('/app/admin', appAdmin.index);
+router.get('/get', appAdmin.index);
 
 
 module.exports = router;
